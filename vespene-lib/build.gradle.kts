26f07b64c7b4551f72f0c071b551649ac4151ce3
Martin Bonnin <martin@mbonnin.net>
2024-07-13 16:22:46 +0200

    bump versions

--- vespene-lib/build.gradle.kts
+++ vespene-lib/build.gradle.kts
@@ -4,21 +4,20 @@ plugins {
   id("signing")
   kotlin("jvm")
   kotlin("kapt")
-  id("net.mbonnin.one.eight")
 }
 
 dependencies {
-  implementation("com.squareup.okhttp3:okhttp:4.9.12")
-  api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+  implementation("com.squareup.okhttp3:okhttp:4.10.0")
+  api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
   api("com.squareup.retrofit2:retrofit:2.11.0")
   implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
-  implementation("org.bouncycastle:bcprov-jdk15on:1.70")
-  implementation("org.bouncycastle:bcpg-jdk15on:1.64")
-  implementation("com.squareup.moshi:moshi:1.11.0")
+  implementation("org.bouncycastle:bcprov-jdk18on:1.78.1")
+  implementation("org.bouncycastle:bcpg-jdk18on:1.78.1")
+  implementation("com.squareup.moshi:moshi:1.15.1")
 
-  kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
+  kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 
-  testImplementation("junit:junit:4.14")
+  testImplementation("junit:junit:4.13.2")
 }
 
 configurePublishing()
