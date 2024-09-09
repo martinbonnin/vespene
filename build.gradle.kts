0a8c5cb4fa895d90877c6c90d866483ae0fa4b92
Martin Bonnin <martin@mbonnin.net>
2024-09-09 15:18:45 +0200

    update vespene

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.gradleup.librarian.gradle.librarianRoot
 plugins {
   id("org.jetbrains.kotlin.jvm").version("2.0.0").apply(false)
   id("com.google.devtools.ksp").version("2.0.0-1.0.23").apply(false)
-  id("com.gradleup.librarian").version("0.0.5")
+  id("com.gradleup.librarian").version("0.0.6")
 }
 
 librarianRoot()
